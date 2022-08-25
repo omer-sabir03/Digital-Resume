f036ff0a6dff99b77e7b7a7ea794ca56158233de
omer-sabir03 <105328315+omer-sabir03@users.noreply.github.com>
2022-08-25 20:44:18 +0530

    Update

--- resume.js
+++ resume.js
@@ -1,6 +1,7 @@
 const open = document.getElementById("open");
 const close = document.getElementById("close");
 const content = document.querySelector(".content");
+const nav = document.querySelector(".show-nav");
 
 open.addEventListener("click", () => content.classList.add("show-nav"));
 close.addEventListener("click", () => content.classList.remove("show-nav"));
